fd3423861dc5b85e7fc1e1120f05636331fd6f59
YiWangWuJi <yb@ngbit.com>
2015-07-10 00:55:04 +0800

    临时提交一些文件
    临时提交未完成，页面load方式需改动，有一些接口无法调用或调用出现错误，还未沟通解决

--- homeCloud-web/js/haier-common.js
+++ homeCloud-web/js/haier-common.js
@@ -1,4 +1,19 @@
 ﻿/**
+ *  引用公共js-rpc
+ *  @see 供页面单独调用使用
+ *  @author renwei
+ */
+document.write('<script type="text/javascript" src="js/jsonrpc.js"></script>');
+document.write('<script type="text/javascript" src="js/common-rpc.js"></script>');
+/**
+ * 临时写死调用rpc的url(业务类型和token需要自己拼接)
+ */
+var rpcUrl = 'http://192.168.68.2/api/v1/';
+/**
+ * 临时写死token
+ */
+var token = '?token=5d5f6e93b7f042a89927cd5c1e4d4d81';
+/**
 *	common javascript.
 *	@Author Nlwy update 2015-07-04.
 *	Copyright haier 2015.
@@ -9,13 +24,32 @@ var common = {
 	*	dom集合
 	*/
 	el : {
-		navLi : "nav ul li",//首页导航
+		navLi : "nav",			//首页导航
+		radio : ".radio_list",	//单选按钮
+		radioc: ".radiocheck",	//选择按钮
+		pw	  : ".password", 	//密码输入框
+		check : ".checkbox", 	//开关按钮
+		select: ".select",		//下拉框
+		iptext: ".iptext",		//DNS地址
+		slide : "i.open"		//收缩箭头
+	},
+	/*
+	*	页面加载完需要运行的方法不包括图片realy
+	*/
+	showReady: function(){
+		this.functions.loadNavHeader();
 	},
 	/*
 	*	页面加载完需要运行的方法包括图片load
 	*/
 	showLoad : function(){
 		this.events.navEvent();
+		this.events.radioList();
+		this.events.passWord();
+		this.events.checkBox();
+		this.events.selectList();
+		this.events.ipTextEmpty();
+		this.events.slideUL();
 		this.functions.getNavIndex();
 	},
 	/*
@@ -27,6 +61,13 @@ var common = {
 		*/
 		getNavIndex : function(){
 			if(common.localdata.getSiData("index")){$(common.el.navLi).eq(parseInt(common.localdata.getSiData("index"))).addClass("now");}
+		},
+		/*
+		*	加载导航和头部
+		*/
+		loadNavHeader: function(){
+			$("nav").load("nav.html");
+			$("#header").load("header.html");
 		}
 	},
 	/*
@@ -37,18 +78,85 @@ var common = {
 		*	首页导航切换
 		*/
 		navEvent : function(){
-			if($(common.el.navLi)){
-				$(common.el.navLi).on("click",function(){
-					if(!$(this).hasClass("now")){
-						try{
-							common.localdata.setSiData("index",$(this).index());
-						}catch(e){
-							//alert(e);
-						}
-						$(this).siblings().removeAttr("class").end().addClass("now");
-					}
-				});
-			}
+			$(common.el.navLi).on("click"," > ul > li",function(){
+				if(!$(this).hasClass("now")){
+					$(this).siblings().removeAttr("class").end().addClass("now");
+				}
+			});
+		},
+		/*
+		*	单选按钮切换
+		*/
+		radioList : function(){
+			$(common.el.radio).on("click","label",function(){
+				if(!$(this).find(".radio").hasClass("selected")){
+					$(common.el.radio).find(".radio").removeClass("selected");
+					$(this).find(".radio").addClass("selected");
+					$("ul[id*=setting_]").hide();
+					$("#setting_"+$(this).index()).show();
+				}
+			});
+			$(common.el.radioc).on("click",function(){
+				$(this).toggleClass("selected");
+			});
+
+		},
+		/*
+		*	密码输入框明文显示
+		*/
+		passWord : function(){
+			$(common.el.pw).find("icon").on({
+				mouseenter : function(){
+					$(this).prev().attr("type","text");
+				},
+				mouseleave: function(){
+					$(this).prev().attr("type","password");
+				}
+			})
+		},
+		/*
+		*	开关按钮
+		*/
+		checkBox : function(){
+			$(common.el.check).on("click",function(){
+				$(this).toggleClass("selected");
+			});
+		},
+		/*
+		*	下拉选择框
+		*/
+		selectList : function(){
+			$(common.el.select).on("click",function(e){
+				e.stopPropagation();
+				if(!$(this).hasClass("open")){
+					$(common.el.select).removeClass("open");
+					$("aside").slideUp(100);
+				};
+				$(this).toggleClass("open");
+				$(this).find("aside").slideToggle(100);
+			}).find("a").on("click",function(){
+				$(this).parent().parent().find("h3").html($(this).html()+"<icon></icon>");
+			});
+			$(window).on("click",function(){
+				$(common.el.select).removeClass("open").find("aside").slideUp(100);
+			});
+		},
+		/*
+		*	DNS清零
+		*/
+		ipTextEmpty : function(){
+			$(common.el.iptext).find("icon").on("click",function(){
+				$(this).prev().find("input").val("");
+			});
+		},
+		/*
+		*	收缩展开
+		*/
+		slideUL : function(){
+			$(common.el.slide).parent().on("click",function(){
+				$(this).find("i").toggleClass("down");
+				$(this).parent().next().slideToggle(300);
+			});
 		}
 	},
 	history : {
@@ -97,7 +205,7 @@ var common = {
 
 			switch(index){
 				case 0 :
-					this.pushHistory({page : 1},"WAN设置页","#recharge");
+					//this.pushHistory({page : 1},"WAN设置页","#");
 				break;
 				default :
 					return false;
@@ -138,6 +246,9 @@ var common = {
 	}
 
 };
+$(document).on("ready",function(){
+	common.showReady();
+});
 $(window).on("load",function(){
 	common.showLoad();
 });
---
+++ homeCloud-web/js/wan-setting.js
@@ -0,0 +1,102 @@
+﻿/**
+ * wan-setting相关js
+ * @author renwei
+ */
+var wanIndex = {
+		/**
+		 * 业务相关集合
+		 */
+		paramOperate : {
+			/**
+			 * 业务类型
+			 */
+			businessType : 'router',
+			/**
+			 * 方法集合
+			 */
+			method : {
+				getWanConnectInfo : 'WAN.getConnectInfo',//获取wan连接信息method
+				setWanConnectInfo : 'WAN.setConnectInfo',//设置wan连接信息method
+				setOnDemandMode   : 'WAN.setOnDemandMode',//使能/关闭按需拨号模式
+				notifyIfaceCreatedInfo : 'WAN.notifyIfaceCreatedInfo',//wan口连接建立通知信息
+				notifyIfaceLostInfo : 'WAN.notifyIfaceLostInfo',//wan口连接失败通知信息
+				getIfaceBaseInfo : 'WAN.getIfaceBaseInfo',//获取wan口基本信息
+				getTrafficInfo : 'WAN.getTrafficInfo',//获取wan口实时流量
+				setDnsInfo : 'WAN.setDnsInfo',//设置静态dns配置信息
+				getDnsInfo : 'WAN.getDnsInfo'//获取静态dns配置信息
+			}
+		},
+		/**
+		 * 初始化加载
+		 */
+		init : function() {
+			this.getWanConnectInfo();
+		},
+		/**
+		 * 获取wan连接信息
+		 */
+		getWanConnectInfo : function() {
+			//这里需要加等待效果
+			rpcUrl = rpcUrl + this.paramOperate.businessType;
+			callRpc(rpcUrl + token, this.paramOperate.method.getWanConnectInfo, null, this.wanConnectInfoCallBack);
+		},
+		/**
+		 * wan获取连接信息回调
+		 */
+		wanConnectInfoCallBack : function(data) {
+			wanIndex.echoWanConnectInfo(data);
+		},
+		/**
+		 * 回显wan连接信息
+		 */
+		echoWanConnectInfo : function(data) {
+			console.log(data);
+			var index;
+			switch (data.protocol) {
+			case 'dhcp':
+				index = 0;
+				break;
+			case 'pppoe':
+				$('input[name="userName"]').val(data.info.username);
+				$('input[name="password"]').val(data.info.password);
+				$('input[name="userName"]').val(data.info.username);
+				switch (data.info.mode) {
+				case 'always':
+					$('#connect_mode').find('h3').text($('#auto_con').text() + '<icon></icon>');
+					break;
+				case 'manual':
+					$('#connect_mode').find('h3').text($('#manual_con').text() + '<icon></icon>');
+					break;
+				case 'demand':
+					$('#connect_mode').find('h3').text($('#need_con').text() + '<icon></icon>');
+					$('input[name="idletime"]').val(data.info.idletime);
+					break;
+				}
+				index = 1;
+				break;
+			case 'static':
+				index = 2;
+				break;
+			case 'wirelessRelay'://临时模拟写的，文档里面没有
+				index = 3;
+				break;
+			}
+			var wanSetting = $('#setting_' + index);
+			$('.radio_list').find(".radio").removeClass("selected");
+			$('.radio_list').find(".radio").eq(index).addClass("selected");
+			wanSetting.show();
+		},
+		/**
+		 * 获取wan口基本信息
+		 */
+		getIfaceBaseInfo : function() {
+			
+		}
+}
+
+/**
+ * dom加载完成即执行 
+ */
+$(function(){
+	wanIndex.init();
+});
